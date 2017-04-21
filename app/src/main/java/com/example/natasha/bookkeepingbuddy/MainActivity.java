d4a672c73b5ad488ed5a6083da11adf59579e4f5
nlcortez <natashalcortez95@gmail.com>
2017-04-21 10:18:39 -0700

    Added database helper

--- app/src/main/java/com/example/natasha/bookkeepingbuddy/MainActivity.java
+++ app/src/main/java/com/example/natasha/bookkeepingbuddy/MainActivity.java
@@ -1,13 +1,58 @@
 package com.example.natasha.bookkeepingbuddy;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.natasha.bookkeepingbuddy.data.BookkeepingContract;
+import com.example.natasha.bookkeepingbuddy.data.BookkeepingDbHelper;
 
 public class MainActivity extends AppCompatActivity {
+    private SQLiteDatabase bookkeepingDB;
+
+    private EditText mNewCategoryNameEditText;
+    private EditText mNewUnitEditText;
+    private Button mBtnAddCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNewCategoryNameEditText = (EditText) findViewById(R.id.cat_name_edit_text);
+        mNewUnitEditText = (EditText) findViewById(R.id.cat_unit_edit_text);
+        mBtnAddCategory = (Button)findViewById(R.id.save_button);
+
+        BookkeepingDbHelper dbHelper = new BookkeepingDbHelper(this);
+        bookkeepingDB = dbHelper.getWritableDatabase();
+
+        addToBook();
+    }
+
+    public void addToBook() {
+        mBtnAddCategory.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String category = mNewCategoryNameEditText.getText().toString();
+                        String unit = mNewUnitEditText.getText().toString();
+
+                        addNewCategory(category, unit);
+                    }
+                }
+        );
+    }
+
+    private long addNewCategory(String name, String unit) {
+        ContentValues cv = new ContentValues();
+
+        cv.put(BookkeepingContract.CategoryEntry.COLUMN_CATEGORY_NAME, name);
+        cv.put(BookkeepingContract.CategoryEntry.COLUMN_UNIT, unit);
+
+        return bookkeepingDB.insert(BookkeepingContract.CategoryEntry.TABLE_NAME, null, cv);
     }
 }
---
+++ app/src/main/java/com/example/natasha/bookkeepingbuddy/data/BookkeepingDbHelper.java
@@ -0,0 +1,38 @@
+package com.example.natasha.bookkeepingbuddy.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.natasha.bookkeepingbuddy.data.BookkeepingContract.*;
+/**
+ * Created by Natasha on 4/21/2017.
+ */
+
+public class BookkeepingDbHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "bookkeeping.db";
+
+    private static final int DATABASE_VERSION = 1;
+
+    public BookkeepingDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_BOOKKEEPING_TABLE = "CREATE TABLE " +
+                CategoryEntry.TABLE_NAME + " (" +
+                CategoryEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                CategoryEntry.COLUMN_CATEGORY_NAME + " TEXT NOT NULL, " +
+                CategoryEntry.COLUMN_UNIT + " TEXT NOT NULL" +
+                "); ";
+        db.execSQL(SQL_CREATE_BOOKKEEPING_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + CategoryEntry.TABLE_NAME);
+        onCreate(db);
+    }
+
+}
