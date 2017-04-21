0bf6516cbf864dcbd2783c5b89d211c825b0668f
nlcortez <natashalcortez95@gmail.com>
2017-04-21 08:34:06 -0700

    Added contract class for database

---
+++ app/src/main/java/com/example/natasha/bookkeepingbuddy/data/BookkeepingContract.java
@@ -0,0 +1,17 @@
+package com.example.natasha.bookkeepingbuddy.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Natasha on 4/21/2017.
+ */
+
+public final class BookkeepingContract {
+    private BookkeepingContract() {}
+
+    public static final class CategoryEntry implements BaseColumns {
+        public static final String TABLE_NAME = "categories";
+        public static final String COLUMN_CATEGORY_NAME = "name";
+        public static final String COLUMN_UNIT = "unit";
+    }
+}
