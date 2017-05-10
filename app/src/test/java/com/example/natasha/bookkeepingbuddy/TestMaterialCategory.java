b77d420c4f7b8f3d33ff50ba06749d2d4c61d000
nlcortez <natashalcortez95@gmail.com>
2017-05-10 14:40:25 -0700

    Fixing travis again

--- app/src/test/java/com/example/natasha/bookkeepingbuddy/TestMaterialCategory.java
+++ app/src/test/java/com/example/natasha/bookkeepingbuddy/TestMaterialCategory.java
@@ -1,5 +1,6 @@
 package com.example.natasha.bookkeepingbuddy;
 
+import com.example.natasha.bookkeepingbuddy.model.Material;
 import com.example.natasha.bookkeepingbuddy.model.MaterialCategory;
 
 import org.junit.Test;
@@ -13,11 +14,43 @@ import static org.junit.Assert.assertNotNull;
 
 public class TestMaterialCategory {
   @Test
-  public void constructor_isNotNull() throws Exception {
+  public void testConstructor() throws Exception {
     MaterialCategory category = new MaterialCategory("yarn", "yards");
 
     assertNotNull(category);
   }
 
+  @Test
+  public void testGetName() throws Exception {
+    String name = "yarn";
+    MaterialCategory category = new MaterialCategory(name, "yards");
 
+    assertEquals(name, category.getName());
+  }
+
+  @Test
+  public void testSetName() throws Exception {
+    MaterialCategory category = new MaterialCategory("thread", "yards");
+    String name = "yarn";
+    category.setName(name);
+
+    assertEquals(name, category.getName());
+  }
+
+  @Test
+  public void testGetUnit() throws Exception {
+    String unit = "yards";
+    MaterialCategory category = new MaterialCategory("yarn", unit);
+
+    assertEquals(unit, category.getUnit());
+  }
+
+  @Test
+  public void testSetUnit() throws Exception {
+    MaterialCategory category = new MaterialCategory("thread", "feet");
+    String unit = "yards";
+    category.setUnit(unit);
+
+    assertEquals(unit, category.getUnit());
+  }
 }
